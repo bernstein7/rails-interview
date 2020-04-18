422c81cd99b0d9841bc1f99ce0c785b0c7803c3e
bernstein <clapandslap@gmail.com>
2020-04-18 19:31:26 +0300

    Add Answers field to QueryType

--- app/graphql/types/answer_type.rb
+++ app/graphql/types/answer_type.rb
@@ -4,5 +4,15 @@ module Types
     field :body, String, null: true
     field :created_at, Types::DateTimeType, null: true
     field :updated_at, Types::DateTimeType, null: true
+    field :user, UserType, null: :false
+    field :question, QuestionType, null: :false
+
+    def user
+      Loaders::AssociationLoader.for(Answer, :user).load(object)
+    end
+
+    def question
+      Loaders::AssociationLoader.for(Answer, :question).load(object)
+    end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,17 +3,22 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :answers, AnswerType.connection_type, null: false
     field :users, UserType.connection_type, null: false do
       argument :has_answers, GraphQL::Types::Boolean, required: false
     end
     field :questions, QuestionType.connection_type, null: false
 
-    def questions
-      Question.all
+    def answers
+      Answer.all
     end
 
     def users(has_answers: nil)
       User.merge(Filters::User::HasAnswers.new(has_answers).relation)
+    end
+
+    def questions
+      Question.all
     end
   end
 end
